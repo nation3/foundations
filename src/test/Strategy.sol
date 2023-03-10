b16ca4dea27ec33d5eb4a5746158cf2822c54b20
q1ke.eth <q1keonchain@proton.me>
2023-03-10 14:50:15 +0100

    Update tests to reflect new representative feature

--- src/test/Strategy.sol
+++ src/test/Strategy.sol
@@ -8,7 +8,7 @@ import {IVotingEscrow, Point} from "../governance/IVotingEscrow.sol";
 
 import {VotingStrategy} from "../governance/VotingStrategy.sol";
 
-contract IntegrationTest is DSTestPlus {
+contract StrategyTest is DSTestPlus {
     Vm public vm = Vm(HEVM_ADDRESS);
 
     string public MAINNET_RPC_URL = "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
@@ -26,20 +26,20 @@ contract IntegrationTest is DSTestPlus {
         strategy = new VotingStrategy();
     }
 
-    function testCheckpoint() public {
+    function testSupplyRepresentingSelf() public {
         console2.log("Supply Point Epoch: %s", strategy.getSupplyPointEpoch());
 
-        strategy._checkpoint(address2);
+        strategy._checkpoint(address1, address1);
 
         uint256 supplyPoint = strategy.totalSupply();
 
         console2.log("Total voting power: %s", supplyPoint);
 
-        assertEq(supplyPoint, veToken.balanceOf(address2));
+        assertEq(supplyPoint, veToken.balanceOf(address1));
 
         console2.log("Supply Point Epoch: %s", strategy.getSupplyPointEpoch());
 
-        strategy._checkpoint(address1);
+        strategy._checkpoint(address2, address2);
 
         console2.log("Supply Point Epoch: %s", strategy.getSupplyPointEpoch());
 
@@ -47,17 +47,17 @@ contract IntegrationTest is DSTestPlus {
 
         console2.log("Total voting power: %s", supplyPoint);
 
-        uint256 sumBalance = veToken.balanceOf(address2) + veToken.balanceOf(address1);
+        uint256 sumBalance = veToken.balanceOf(address1) + veToken.balanceOf(address2);
 
         console2.log("Sum of balances %s", sumBalance);
 
         assertEq(supplyPoint, sumBalance);
     }
 
-    function testCheckpoint2() public {
+    function testSupplyWithIncreasedLockRepresentingSelf() public {
         console2.log("Supply Point Epoch: %s", strategy.getSupplyPointEpoch());
 
-        strategy._checkpoint(address1);
+        strategy._checkpoint(address1, address1);
 
         uint256 supplyPoint = strategy.totalSupply();
 
@@ -69,7 +69,7 @@ contract IntegrationTest is DSTestPlus {
 
         veToken.increase_unlock_time(1709679600);
 
-        strategy._checkpoint(address1);
+        strategy._checkpoint(address1, address1);
 
         supplyPoint = strategy.totalSupply();
 
@@ -80,11 +80,11 @@ contract IntegrationTest is DSTestPlus {
         console2.log("Supply Point Epoch: %s", strategy.getSupplyPointEpoch());
     }
 
-    function testCheckpoint3() public {
+    function testSupplyWithTwoAddressesWithIncreasedLockRepresentingSelf() public {
         console2.log("Supply Point Epoch: %s", strategy.getSupplyPointEpoch());
 
-        strategy._checkpoint(address2);
-        strategy._checkpoint(address1);
+        strategy._checkpoint(address1, address1);
+        strategy._checkpoint(address2, address2);
 
         console2.log("Balance before: %s", veToken.balanceOf(address1));
 
@@ -93,39 +93,39 @@ contract IntegrationTest is DSTestPlus {
 
         console2.log("Balance after: %s", veToken.balanceOf(address1));
 
-        strategy._checkpoint(address1);
+        strategy._checkpoint(address1, address1);
 
         uint256 supplyPoint = strategy.totalSupply();
 
-        uint256 sumBalance = veToken.balanceOf(address2) + veToken.balanceOf(address1);
+        uint256 sumBalance = veToken.balanceOf(address1) + veToken.balanceOf(address2);
 
         console2.log("Sum of balances %s", sumBalance);
 
         assertEq(supplyPoint, sumBalance);
     }
 
-    function testBalanceOf() public {
-        strategy._checkpoint(address1);
+    function testBalanceOfWithOneAddressRepresentingSelf() public {
+        strategy._checkpoint(address1, address1);
 
         assertEq(strategy.balanceOf(address1), veToken.balanceOf(address1));
     }
 
-    function testBalanceOfAt() public {
-        strategy._checkpoint(address1);
+    function testBalanceOfDoesNotGiveVotingPowerRetroactively() public {
+        strategy._checkpoint(address1, address1);
 
         // Checkpoint was made now. There is no voting power 100 block ago.
         assertEq(strategy.balanceOfAt(address1, block.number - 100), 0);
     }
 
-    function testBalanceOfAt2() public {
-        strategy._checkpoint(address1);
+    function testBalanceOfAdjustsVotingPowerForward() public {
+        strategy._checkpoint(address1, address1);
         vm.roll(block.number + 100);
 
         assertEq(strategy.balanceOfAt(address1, block.number), veToken.balanceOfAt(address1, block.number));
     }
 
     function testTotalSupply() public {
-        strategy._checkpoint(address1);
+        strategy._checkpoint(address1, address1);
         vm.roll(block.number + 100);
 
         console2.log("Total supply at %s: %s", block.number, strategy.totalSupplyAt(block.number));
@@ -134,9 +134,9 @@ contract IntegrationTest is DSTestPlus {
         assertEq(strategy.balanceOfAt(address1, block.number), strategy.totalSupplyAt(block.number));
     }
 
-    function testTotalSupply2() public {
-        strategy._checkpoint(address1);
-        strategy._checkpoint(address2);
+    function testTotalSupplyAdjustsVotingPowerForward() public {
+        strategy._checkpoint(address1, address1);
+        strategy._checkpoint(address2, address2);
 
         vm.roll(block.number + 100);
 
